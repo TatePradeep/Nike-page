e9afceefa597d34e69ef76fd1b8bc923c5b32d4c
TatePradeep <pradeeptate87836@gmail.com>
2024-01-31 18:05:31 +0530

    version1

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+import CustomerReview from './CustomerReview'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export{Hero,PopularProducts,SuperQuality,Services,SpecialOffer,CustomerReview,Subscribe,Footer}
